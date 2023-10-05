d8d8658bf7d3d4a77352232db94ab3fe0ab97879
Nicolas Valencia <dnikos000@gmail.com>
2023-10-04 21:51:53 -0500

    Instagram

---
+++ proyecto/js/mas.js
@@ -0,0 +1,6 @@
+const masContainer = document.querySelector(".menu_mas");
+const mas = document.querySelector(".mas");
+
+mas.addEventListener("click", () => {
+masContainer.classList.toggle("actives");
+});
